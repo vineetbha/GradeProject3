15881ba1cdbadfad096109189f0fae54b567d48f
Vineet Bhavsar <vineetbhavsar1997@gmail.com>
2022-06-14 15:13:23 +0530

    First Commit in GradeProject3

---
+++ src/com/nt/gl/Driver.java
@@ -0,0 +1,28 @@
+package com.nt.gl;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Driver {
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("enter the total no of floors in the building");
+		
+		int noOfFloor = sc.nextInt();
+
+	    int[] floors = new int[noOfFloor];
+		
+	   
+		for(int i=0; i<noOfFloor; i++)  
+		{  
+		System.out.println("enter the floor size given on day : "+(i+1));
+		floors[i] = sc.nextInt(); 
+		}  
+		System.out.println();
+		
+		Service service = new Service();
+		service.printConstructionTable(floors, noOfFloor);
+	}
+}
